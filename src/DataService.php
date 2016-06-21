021db2e68b48799e89cd48cfea04b4a435085385
Nathan <nathanaqd@gmail.com>
2016-06-21 09:48:29 +0100

    fixed minor version

--- src/DataService.php
+++ src/DataService.php
@@ -150,7 +150,7 @@ class DataService
         $uri = $this->getRequestUrl($this->entity);
 
         if ($minorVersion !== null) {
-            $uri = $uri . '&minorversion=' . $minorVersion;
+            $uri = $uri . '?minorversion=' . $minorVersion;
         }
 
         return $this->request('POST', $uri, $payload);
@@ -180,7 +180,7 @@ class DataService
         $uri = $this->getRequestUrl($this->entity) . '?operation=update';
 
         if ($minorVersion !== null) {
-            $uri = $uri . '&minorversion=' . $minorVersion;
+            $uri = $uri . '?minorversion=' . $minorVersion;
         }
 
         return $this->request('POST', $uri, $payload);
